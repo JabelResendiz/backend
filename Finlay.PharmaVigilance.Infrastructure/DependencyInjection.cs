5bf1717783cb01b2b5e27787f97f2716da87d38a
JabelResendiz <jabelresendiz00@gmail.com>
2026-04-28 15:06:50 -0400

    update add auth

--- Finlay.PharmaVigilance.Infrastructure/DependencyInjection.cs
+++ Finlay.PharmaVigilance.Infrastructure/DependencyInjection.cs
@@ -195,22 +195,41 @@ public static class DependencyInjection
         services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 
         // Configuración de autenticación JWT
-        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = jwtSettings.Issuer,
-                        ValidAudience = jwtSettings.Audience,
-                        IssuerSigningKey = new SymmetricSecurityKey(
-                            System.Text.Encoding.UTF8.GetBytes(jwtSettings.Secret))
-                    };
-                });
+        // services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        //         .AddJwtBearer(options =>
+        //         {
+        //             options.TokenValidationParameters = new TokenValidationParameters
+        //             {
+        //                 ValidateIssuer = true,
+        //                 ValidateAudience = true,
+        //                 ValidateLifetime = true,
+        //                 ValidateIssuerSigningKey = true,
+        //                 ValidIssuer = jwtSettings.Issuer,
+        //                 ValidAudience = jwtSettings.Audience,
+        //                 IssuerSigningKey = new SymmetricSecurityKey(
+        //                     System.Text.Encoding.UTF8.GetBytes(jwtSettings.Secret))
+        //             };
+        //         });
 
+        services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings.Issuer,
+        ValidAudience = jwtSettings.Audience,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(jwtSettings.Secret))
+    };
+});
         return services;
     }
 
