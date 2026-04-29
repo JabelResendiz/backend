d79136a01e9121acfc8b14b7251bfd52207c9ba4
JabelResendiz <jabelresendiz00@gmail.com>
2026-04-28 22:10:35 -0400

    &

--- Finlay.PharmaVigilance.Application/DTOs/Report/Response/ReportSummaryDto.cs
+++ Finlay.PharmaVigilance.Application/DTOs/Report/Response/ReportSummaryDto.cs
@@ -1,9 +1,12 @@
 
+using Finlay.PharmaVigilance.Domain.Enum;
+
 namespace Finlay.PharmaVigilance.Application.DTO;
 
 public class ReportSummaryDto
 {
     public required DateTime ReportDate { get; set; }
+    public required ReportStatus Status { get; set; }
     public required VaccinatedSubjectSummaryDto VaccinatedSubject { get; set; }
     public required IEnumerable<VaccinationSummaryDto> Vaccinations { get; set; }
     public required IEnumerable<AdverseEventSummaryDto> AdverseEvents { get; set; }
